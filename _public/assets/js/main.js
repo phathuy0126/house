7dd2abf87c5d7abbd7ac5d016218869513f6aa02
DESKTOP-HVB515S\PhatHuy <huynh_phat_huy@allgrow-labo.jp>
2023-03-15 15:15:43 +0700

    up code

---
+++ _public/assets/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $('.c-slider').slick({
+    prevArrow:'<img src="./assets/img/sliderleft.png" alt="slider" class="c-slider__prev">',
+    nextArrow: '<img src="./assets/img/sliderleft.png" alt="slider" class="c-slider__next">'
+  });
+});
